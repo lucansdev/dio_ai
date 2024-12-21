b10da070e718ec653e7090d3287eaeea4773787f
lucansdev <146260839+lucansdev@users.noreply.github.com>
2024-12-20 23:11:48 -0300

    Add files via upload

---
+++ aula3.r
@@ -0,0 +1,13 @@
+print("hello world")
+
+objeto <- 3*3
+
+print(objeto)
+
+raio<- 10
+espessura <- 1
+comprimento <- 70
+
+valume <- pi*(raio - espessura)^2*comprimento
+
+print(valume)
---
+++ aula4.r
@@ -0,0 +1,14 @@
+primeiro_vetor <- c(1, 2, 3, 4, 5)
+primeiro_vetor2 <- c(1, 3, 6, 7, 9)
+
+primeiro_vetor3 <- c(primeiro_vetor, primeiro_vetor2)
+
+print(primeiro_vetor3)
+
+matriz_a <- matrix(c(1,2,3,4,5,6),nrow = 2,ncol = 3,byrow = TRUE)
+
+print(matriz_a)
+
+x <- c(1,2,3,4,5,6,7,7,7,8,9)
+
+print((x))
